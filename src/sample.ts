a3500cce2da1ac2bea41875cd06e4cea691c4021
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-22 23:16:01 +0900

    completed reverseArray() in sample.ts

--- src/sample.ts
+++ src/sample.ts
@@ -1,12 +1,20 @@
 export const hoge = () => {
     console.log("hogehoge");
-
-
-
+    console.log(reverseArray(["a", "b", "c"], true));
 };
+
+
 // 引数の文字列の配列を逆にして返す関数reverseArrayを作成。
 //  a, b, c を入れたら c, b, aを返す。
-//  a, b, c, dをいれたら　d, c, b, aを返す。
-export function reverseArray(){
-    return ["c", "b", "a"];
+// hasTomeはオプション引数
+export function reverseArray(arr: string[], hasTom?: boolean): string[] {
+    const result: string[] = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[arr.length - i - 1]);
+        
+    }
+    if (hasTom) {
+        result.push("Tom");
+    }
+    return result;
 };
--- test/sample.test.ts
+++ test/sample.test.ts
@@ -2,10 +2,10 @@ import { reverseArray } from "../src/sample"
 
 
 test("a, b, cをいれたらc, b, aを返す", () => {
-    expect(reverseArray()).toEqual(["c", "b", "a"]);
+    expect(reverseArray(["a", "b", "c"])).toEqual(["c", "b", "a"]);
 });
 
-// test("a, b, c, dをいれたらd, c, b, aを返す", () => {
-//     const result 
-// }
+test("a, b, c, dをいれたらd, c, b, aを返す", () => {
+    expect(reverseArray(["a", "b", "c", "d"])).toEqual(["d", "c", "b", "a"]);
+});
 
