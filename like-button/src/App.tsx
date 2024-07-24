0039aeabdf505feb6d09e86b0f90c7963b31ddc1
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-24 14:59:24 +0900

    added LikeButton() in App.tsx

--- like-button/src/App.tsx
+++ like-button/src/App.tsx
@@ -5,10 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">typescriptはいいぞ
+      <header className="App-header">
+        <LikeButton />
+
       </header>
     </div>
   );
+}
+
+function LikeButton() {
+  return <span>いいねボタン予定地</span>
 }
 
 export default App;
