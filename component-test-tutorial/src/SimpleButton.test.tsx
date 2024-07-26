96900043cc4f3822d76a54ae92964cdac348dffe
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-27 00:51:12 +0900

    added new test

--- component-test-tutorial/src/SimpleButton.test.tsx
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -1,13 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { UserEvent } from "@testing-library/user-event";
 import { SimpleButton } from "./SimpleButton";
+import userEvent from "@testing-library/user-event";
 
 test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
-
-    // ここにテストの内容をかいていきます
+    const user = userEvent.setup();
     render(<SimpleButton />);
 
     const simpleButton = screen.getByRole("button");
     expect(simpleButton).toHaveTextContent("OFF");
+    await userEvent.click(simpleButton);
+    expect(simpleButton).toHaveTextContent("ON"); 
 
 
 });
