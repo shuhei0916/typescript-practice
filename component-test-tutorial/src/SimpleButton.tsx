3968b756c5c85cbee25a4b2ecfd438f3bacc4a8c
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-28 15:01:34 +0900

    modified line 4 and ensured that test failed

--- component-test-tutorial/src/SimpleButton.tsx
+++ component-test-tutorial/src/SimpleButton.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const SimpleButton: () => JSX.Element = () => {
-    const [state, setState] = useState(false);
+    const [state, setState] = useState(true);
     const handleClick = () => {
         setState((prevState) => !prevState);
     };
