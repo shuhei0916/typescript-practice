76203f98190eba839b89c8e2381cca551e96cc4f
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-28 14:55:17 +0900

    added snapshot test

--- component-test-tutorial/src/SimpleButton.test.tsx
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -12,5 +12,9 @@ test("ボタンをクリックするとON/OFFの表示が切り替わる", async
     await userEvent.click(simpleButton);
     expect(simpleButton).toHaveTextContent("ON"); 
 
+});
 
+test("描画されてすぐはOFFと表示されている", () => {
+    const view = render(<SimpleButton/>);
+    expect(view.container).toMatchSnapshot();
 });
